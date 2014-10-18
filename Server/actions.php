3f2bb8448f2a8e3ea0268bfe7621e1430fbaf17f
jchristmann <johannes.christmann@gmx.net>
2014-10-18 18:13:35 +0200

    fetch sensor values #1

--- Server/actions.php
+++ Server/actions.php
@@ -1,11 +1,11 @@
 <?php
 
-function execute_action($action)
+function execute_action($action, $data)
 {
 	switch($action)
 	{
-		case "getData":
-			getData();
+		case "getSensorData":
+			echo getSensorData($data);
 		break;
 		case "sensors":
 			echo getSensors();
@@ -15,13 +15,37 @@ function execute_action($action)
 	}
 }
 
-function getData()
-{
-	$res = mysql_query("SELECT Sensors.NAME as SENSOR_NAME, SensorValues.VALUE, SensorValues.TIMESTAMP FROM SensorValues LEFT JOIN Sensors ON (SensorValues.SENSOR_ID=Sensors.ID)");
-	while($row = mysql_fetch_array($res, MYSQL_ASSOC))
+function getSensorData($data)
+{	
+	$data = null;
+	if(isset($data["sensorName"]))
 	{
-		echo $row["SENSOR_NAME"] . "@" . $row["TIMESTAMP"] . ": " . $row["VALUE"] . "</br>";
+		$data = getSingleSensorData($data["sensorName"]);
 	}
+	else
+	{
+		$data = getAllSensorData();
+	}
+
+	$json = "{";
+	
+	while($row = mysql_fetch_array($data, MYSQL_ASSOC))
+	{
+		$json = $row["SENSOR_NAME"] . "@" . $row["TIMESTAMP"] . ": " . $row["VALUE"];
+	}
+	
+	$json .= "}";
+	return $json;
+}
+
+function getSingleSensorData($name)
+{
+	return mysql_query("SELECT Sensors.NAME as SENSOR_NAME, SensorValues.VALUE, SensorValues.TIMESTAMP FROM SensorValues LEFT JOIN Sensors ON (SensorValues.SENSOR_ID=Sensors.ID) WHERE Sensors.NAME = $name ORDER BY SensorValues.TIMESTAMP ASC");
+}
+
+function getAllSensorData()
+{
+	return mysql_query("SELECT Sensors.NAME as SENSOR_NAME, SensorValues.VALUE, SensorValues.TIMESTAMP FROM SensorValues LEFT JOIN Sensors ON (SensorValues.SENSOR_ID=Sensors.ID) ORDER BY Sensor.ID ASC, SensorValues.TIMESTAMP ASC");
 }
 
 function getSensors()
@@ -34,7 +58,6 @@ function getSensors()
 	$sensors = "";
 	while($row = mysql_fetch_array($res, MYSQL_ASSOC))
 	{
-		
 		$sensor = "\"" . $row["NAME"] ."\"";
 		if($sensors != "")
 		{
